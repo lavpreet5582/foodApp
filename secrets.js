cd1fb5e568013d492756586debfa20808c050abc
lavpreet5582 <lsv2885655@gmail.com>
2021-12-15 00:41:19 +0530

    Removed

--- secrets.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-     JWT_Key : "lovesinghmanka12345",
-     db_link : "mongodb+srv://LavpreetSingh_5582:Mm8bZFpZd0mfYMmw@cluster0.ppxiu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-     SK:"sk_test_51JZFPnSFfTbnooNghDhqADPobfRdrKScwIG4o749739zwK5OL9VdxNdFjrwkRCb4o2SQntyxNCm3b01wIZgZALXY00yZiu71Px"
-     ,email:"lsv2885657@gmail.com",
-     mailpass:"zctxyettfecjtzfo"
-
-}
-//Mm8bZFpZd0mfYMmw
